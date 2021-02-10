170e6311c105bad51567d98933154ff141567b53
JHamed <64268059+Jhamed7@users.noreply.github.com>
2021-02-10 19:47:25 +0330

    v1.0 ready

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print(math.sqrt(25))
 import os
 print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
 #print("PATH:", os.environ.get('PATH'))
+
+import pandas as pd
+import  seaborn as sns
+
+#sns.pairplot()
